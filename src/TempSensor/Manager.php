3aef462168d85e13c7332c45e9d6575ef5300b33
Marek Sterzik <sterzik@spsostrov.cz>
2024-11-29 13:03:38 +0100

    improve temp sensor manager

--- src/TempSensor/Manager.php
+++ src/TempSensor/Manager.php
@@ -2,15 +2,66 @@
 
 namespace App\TempSensor;
 
+use Exception;
+
 class Manager
 {
-    public function getSensors(): array
+    private ?array $sensors = null;
+    private string $dbFile;
+
+    public function __construct()
+    {
+        $this->dbFile = dirname(__DIR__, 2) . "/var/sensors.json";
+    }
+
+    const TYPES = [
+        "http" => HttpTempSensor::class,
+        "test" => TestTempSensor::class,
+    ];
+
+    private function getSensorsConfig(): array
+    {
+        $content = @file_get_contents($this->dbFile);
+        if (!is_string($content)) {
+            return [];
+        }
+        $content = @json_decode($content, true);
+        if (!is_array($content)) {
+            return [];
+        }
+        return $content;
+    }
+
+    private function instantiateSensor(array $config): TempSensor
+    {
+        $type = $config['type'];
+        $name = $config['name'];
+        if (!isset(self::TYPES[$type])) {
+            throw new Exception("Unknown sensor type: $type");
+        }
+        $class = self::TYPES[$type];
+        unset($config['type']);
+        unset($config['name']);
+        $instance = new $class(...$config);
+        $instance->setName($name);
+        return $instance;
+    }
+
+    private function createSensors(): array
     {
         $sensors = [];
-        $sensors[] = (new TestTempSensor())->setName("s1");
-        $sensors[] = (new HttpTempSensor(
-            "https://home.spsostrov.cz/~sterzik/temp.php"
-        ))->setName("s3");
+        foreach ($this->getSensorsConfig() as $config) {
+            $sensors[] = $this->instantiateSensor($config);
+        }
         return $sensors; 
     }
+
+    public function getSensors(): array
+    {
+        if ($this->sensors === null) {
+            $this->sensors = $this->createSensors();
+        }
+        return $this->sensors;
+    }
+
 }
